c0ead73c0708812f06b33214c7ecf585412f0069
Don McCurdy <dm@donmccurdy.com>
2023-08-20 23:35:02 -0400

    Clean up RGB / RGBA issue.

--- encode.sh
+++ encode.sh
@@ -3,14 +3,16 @@
 RESET='\033[0m'
 GREEN='\033[0;32m'
 
+convert source/png/2d_rgb8.png png32:source/png/2d_rgba8.png
+
 # 2D
 echo -e "${GREEN}Creating 2D textures…${RESET}"
 ktx create --encode uastc --zstd 18 --format R8G8B8_SRGB --assign-oetf srgb --assign-primaries bt709 --generate-mipmap source/png/2d_uastc.png ktx2/2d_uastc.ktx2
 ktx create --encode basis-lz --format R8G8B8_SRGB --assign-oetf srgb --assign-primaries bt709 --generate-mipmap source/png/2d_etc1s.png ktx2/2d_etc1s.ktx2
-ktx create --format R8G8B8_SRGB --zstd 18 --assign-oetf srgb --assign-primaries bt709 --generate-mipmap source/png/2d_rgb8.png ktx2/2d_rgb8.ktx2
-ktx create --format R8G8B8_UNORM --zstd 18 --assign-oetf srgb --assign-primaries bt709 --convert-oetf linear --generate-mipmap source/png/2d_rgb8.png ktx2/2d_rgb8_linear.ktx2
-ktx create --format R16G16B16_SFLOAT --zstd 18 --assign-oetf linear --generate-mipmap source/exr/2d_f16.exr ktx2/2d_rgb16_linear.ktx2
-ktx create --format R32G32B32_SFLOAT --zstd 18 --assign-oetf linear --generate-mipmap source/exr/2d_f32.exr ktx2/2d_rgb32_linear.ktx2
+ktx create --format R8G8B8A8_SRGB --zstd 18 --assign-oetf srgb --assign-primaries bt709 --generate-mipmap source/png/2d_rgba8.png ktx2/2d_rgba8.ktx2
+ktx create --format R8G8B8A8_UNORM --zstd 18 --assign-oetf srgb --assign-primaries bt709 --convert-oetf linear --generate-mipmap source/png/2d_rgba8.png ktx2/2d_rgba8_linear.ktx2
+ktx create --format R16G16B16A16_SFLOAT --zstd 18 --assign-oetf linear --generate-mipmap source/exr/2d_f16.exr ktx2/2d_rgba16_linear.ktx2
+ktx create --format R32G32B32A32_SFLOAT --zstd 18 --assign-oetf linear --generate-mipmap source/exr/2d_f32.exr ktx2/2d_rgba32_linear.ktx2
 toktx --encode astc --zcmp 18 --assign_oetf srgb --genmipmap ktx2/2d_astc_6x6.ktx2 source/png/2d_astc_6x6.png
 
 # 3D
